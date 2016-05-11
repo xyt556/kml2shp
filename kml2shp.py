572a7f04102c98e5e359c3e7e03a7839da86a307
ggiuliani <greg@Gregorys-MacBook-Air.local>
2016-05-11 15:58:52 +0200

    Minor modification of comments

--- kml2shp.py
+++ kml2shp.py
@@ -1,20 +1,18 @@
 # Version: 	1.0.0
 # Author: 	Gregory Giuliani
 #			University of Geneva 
-#			Institute of Environmental Sciences
-#			Climatic Change and Climate Impacts/enviroSPACE
-#			Site de Battelle/Bat.D
-#			7 route de Drize
-#			CH - 1227 Carouge
-#			http://www.unige.ch/climate/Team/Giuliani.html
+#			Institute for Environmental Sciences/enviroSPACE
+#			Uni Carl-Vogt
+#			66 boulevard Carl-Vogt
+#			CH - 1205 Genève
+#			http://www.unige.ch/envirospace/people/giuliani/
 #			http://www.unige.ch/envirospace
 #
 # Python script to convert KML to Shapefile
-# Copyright (C) 2011 Gregory Giuliani
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
